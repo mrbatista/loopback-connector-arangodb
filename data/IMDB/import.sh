5db6ab44ccd30815406c9e1d9e96c4e8f766f9f2
Navid Nikpour <navid@nikpour.com>
2015-07-09 04:13:00 +0200

    - Removed hard coded urlendpoint, username and password from imports-scripts - Added a command line parameter for specifying collection first and then the data file to import, with defaults

--- data/IMDB/import.sh
+++ data/IMDB/import.sh
@@ -1,7 +1,47 @@
 #!/usr/bin/env bash
-#!/bin/bash
+. ../json.sh
 
-${ARANGODB_BIN}arangosh --server.username=connector --server.password=connector --server.database=ConnectorTest --quiet <<EOF
+host="$(json_key 'test' 'arangodb' 'host' < ../../.loopbackrc)"
+port="$(json_key 'test' 'arangodb' 'port' < ../../.loopbackrc)"
+database="$(json_key 'test' 'arangodb' 'database' < ../../.loopbackrc)"
+username="$(json_key 'test' 'arangodb' 'username' < ../../.loopbackrc)"
+password="$(json_key 'test' 'arangodb' 'password' < ../../.loopbackrc)"
+
+cmd_parameters=''
+# set url=host:port, connect via tcp
+if [ -z "$host" ] | [ -z "$port" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.endpoint=tcp://$host:$port "
+fi
+
+# set database
+if [ -z "$database" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.database=$database "
+fi
+
+# username
+if [ -z "$username" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.username=$username "
+fi
+
+# password
+if [ -z "$password" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.password=$password "
+fi
+
+
+${ARANGODB_BIN}arangosh $cmd_parameters --quiet <<EOF
   var db = require("org/arangodb").db;
   var Graph = require("org/arangodb/graph").Graph;
   db._drop("imdb_vertices");
@@ -10,4 +50,4 @@ ${ARANGODB_BIN}arangosh --server.username=connector --server.password=connector 
   new Graph("imdb", "imdb_vertices", "imdb_edges"); 
 EOF
 
-${ARANGODB_BIN}arangorestore --server.username=connector --server.password=connector --server.database=ConnectorTest dump
+${ARANGODB_BIN}arangorestore $cmd_parameters dump
--- data/users/import.sh
+++ data/users/import.sh
@@ -1,8 +1,53 @@
 #!/usr/bin/env bash
-${ARANGODB_BIN}arangosh --server.username=connector --server.password=connector --server.database=ConnectorTest --quiet <<EOF
+. ../json.sh
+. ../helpers.sh
+
+host="$(json_key 'test' 'arangodb' 'host' < ../../.loopbackrc)"
+port="$(json_key 'test' 'arangodb' 'port' < ../../.loopbackrc)"
+database="$(json_key 'test' 'arangodb' 'database' < ../../.loopbackrc)"
+username="$(json_key 'test' 'arangodb' 'username' < ../../.loopbackrc)"
+password="$(json_key 'test' 'arangodb' 'password' < ../../.loopbackrc)"
+
+coll=${1:-users}
+dataFile=${2:-data.json}
+
+cmd_parameters=''
+# set url=host:port, connect via tcp
+if [ -z "$host" ] | [ -z "$port" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.endpoint=tcp://$host:$port "
+fi
+
+# set database
+if [ -z "$database" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.database=$database "
+fi
+
+# username
+if [ -z "$username" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.username=$username "
+fi
+
+# password
+if [ -z "$password" ]
+then
+  cmd_parameters+=''
+else
+  cmd_parameters+="--server.password=$password "
+fi
+
+${ARANGODB_BIN}arangosh $cmd_parameters --quiet <<EOF
   var db = require("org/arangodb").db;
   var Graph = require("org/arangodb/graph").Graph;
   
-  db._drop("users");
+  db._drop("$coll");
 EOF
-arangoimp --file data.json --server.username=connector --server.password=connector --server.database=ConnectorTest --collection=users --create-collection=true --type=json
+arangoimp --file=$dataFile $cmd_parameters --collection=$coll --create-collection=true --type=json
